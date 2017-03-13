44d012ea318bb14455eb1ce6ce3656a92d4ef09e
David <david.diez31@free.fr>
2017-03-13 16:57:41 +0100

    save

--- server/index.js
+++ server/index.js
@@ -1,1 +1,27 @@
 //to do server
+import express from 'express';
+import bodyParser from 'body-parser';
+import uuid from 'node-uuid';
+const app = express();
+// const data = './public/database/tasks.json';
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
+
+
+
+
+
+// app.get('/', function(req, res) {
+//     res.redirect("/index");
+// });
+
+// app.get('/index', function(req, res) {
+//     res.sendFile(path.join(__dirname, '/public', 'index.html'));
+// });
+
+app.listen(3000, () => {
+    console.log('Example app listening on port 3000!');
+});
--- server/public/js/app.js
+++ server/public/js/app.js
@@ -1,5 +1,23 @@
 (() => {
-  const app = {
-    //todo
-  }
+    const app = {
+        //todo
+        donnees: null,
+        init() {
+            this.getTasks();
+        },
+        getTasks() {
+            fetch('/database/tasks.json')
+                .then(res => res.json())
+                .then(data => {
+                    this.donnees = data.taches;
+                    this.sendTache();
+                });
+        },
+        sendTache() {
+            this.donnees.map(tasks => {
+                document.getElementById('list').innerHTML += `<li>${tasks.content}</li>`
+            })
+        }
+    };
+    app.init();
 })();
